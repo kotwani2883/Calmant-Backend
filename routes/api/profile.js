0fc555907401806eb8c39b8aa340504defd785f4
unknown <palakkotwani2883@gmail.com>
2021-06-16 22:37:25 +0530

    Backend Server

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,37 +1,36 @@
-const express=require('express');
+const express = require('express');
 const Profile = require('../../models/Profile');
-const router=express.Router();
-const Profile=require('../../models/Profile');
+const router = express.Router();
 const { route } = require('./users');
 
-router.post('/add-concerns',async(req,res)=>{
-    try{
-        const Profile=await Profile.findOne({user:req.body.uid});
-        profile.concerns=req.body.concerns;
+router.post('/add-concerns', async (req, res) => {
+    try {
+        const Profile = await Profile.findOne({ user: req.body.uid });
+        profile.concerns = req.body.concerns;
         await profile.save();
         return res.json(profile);
-    }catch(err){
+    } catch (err) {
         console.log(err.message);
         res.status(500).send('Server Error');
     }
 });
 
-router.post('/edit-profile',async(req,res)=>{
-    try{
-        const {name,email,gender,phone_no,age,concerns,uid}=req.body;
-        const profile=await Profile.findOne({user:uid});
-        profile.name=name;
-        profile.email=email;
-        profile.gender=gender;
-        profile.phone_no=phone_no;
-        profile.age=age;
-        profile.concerns=concerns;
+router.post('/edit-profile', async (req, res) => {
+    try {
+        const { name, email, gender, phone_no, age, concerns, uid } = req.body;
+        const profile = await Profile.findOne({ user: uid });
+        profile.name = name;
+        profile.email = email;
+        profile.gender = gender;
+        profile.phone_no = phone_no;
+        profile.age = age;
+        profile.concerns = concerns;
         await profile.save();
         return res.json(profile);
-    }catch(err){
+    } catch (err) {
         console.log(err.message);
         res.status(500).send('Server Error');
     }
 });
 
-module.exports=router;
+module.exports = router;
