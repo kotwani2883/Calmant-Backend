31b678812e514c992dcb2dc83db2cd38ae849aad
unknown <palakkotwani2883@gmail.com>
2021-06-17 14:40:08 +0530

    Made Changes

--- models/Profile.js
+++ models/Profile.js
@@ -1,31 +1,31 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 
-const ProfileSchema=new mongoose.Schema({
-    user:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'users'
+const ProfileSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users'
     },
-    name:{
-        type:String,
-        required:true
+    name: {
+        type: String,
+        required: true
     },
-    gender:{
-        type:String,
-        required:true,
+    bio: {
+        type: String
     },
-    phone_no:{
-        type:String
-    },
-    desc:{
-        type:String
-    },
-    regarding:{
-        type:Array
+    gender: {
+        type: String,
+        required: true
     },
     age: {
-     type:String,
-     require:true
+        type: String,
+        required: true
+    },
+    phone_no: {
+        type: String
+    },
+    concerns: {
+        type: Array
     }
 });
 
-module.exports=Profile =mongoose.model('profile',ProfileSchema);
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
